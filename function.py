ec71069484e7d5e730600bb617b2e21161a4c7de
Lere Dakshak Mamman <dakshaklere02@gmail.com>
2025-08-22 11:25:48 +0100

    Feat: implement python function with explicit example

---
+++ function.py
@@ -0,0 +1,83 @@
+def check_even_odd(n_list=[0]):
+    for n in n_list:
+        if n % 2 == 0:
+            print(f"{n} is Even")
+        else:
+            print(f"{n} is Odd")
+
+check_even_odd([1, 2, 3, 4, 5])  
+
+
+def check_even_odd(n=0):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Example usage:
+print(check_even_odd())    
+print(check_even_odd(7))    
+print(check_even_odd(12))   
+
+
+
+
+def count_vowels(text, vowels="aeiou"):
+    text = text.lower()  
+    found_vowels = []    
+
+    for char in text:
+        if char in vowels:
+            found_vowels.append(char)
+
+    return len(found_vowels)
+print(count_vowels("Python is fun"))     
+print(count_vowels("HELLO world"))       
+print(count_vowels("Why?"))             
+
+
+
+
+def count_even(numbers):
+    even_count = 0  
+
+    for num in numbers:
+        if num % 2 == 0: 
+            even_count += 1
+
+    return even_count
+print(count_even([1, 2, 3, 4, 6]))  
+print(count_even([7, 9, 13]))       
+print(count_even([2, 4, 6, 8]))     
+
+
+
+
+def triangle_area(base, height):
+    area = 0.5 * base * height
+    return area
+print(triangle_area(10, 5))   # Output: 25.0
+print(triangle_area(6, 3))    # Output: 9.0
+print(triangle_area(0, 10))   # Output: 0.0
+
+
+
+def check_password_strength(password):
+    if len(password) < 6:
+        return "Weak"
+    elif 6 <= len(password) <= 10:
+        return "Medium"
+    else:
+        has_letter = any(char.isalpha() for char in password)
+        has_digit = any(char.isdigit() for char in password)
+
+        if has_letter and has_digit:
+            return "Strong"
+        else:
+            return "Medium"
+print(check_password_strength("123"))           # Weak
+print(check_password_strength("abcdef"))        # Medium
+print(check_password_strength("abcd1234"))      # Medium
+print(check_password_strength("abcd1234567"))   # Strong
+print(check_password_strength("abcdefghijk"))   # Medium
+
